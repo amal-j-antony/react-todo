948b734992c95602f94d77ea64b98d40c59151a5
amal-j-antony <271721431+amal-j-antony@users.noreply.github.com>
2026-06-17 22:13:57 +0530

    add gitignore

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { addTaskAPI, deleteTaskAPI, getTasksAPI, updateTaskAPI } from './services/allAPI'
+import { addTaskAPI, deleteTaskAPI, getTaskByIdAPI, getTasksAPI, updateTaskAPI } from './services/allAPI'
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/src/ui/dialog"
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 
 function Home() {
@@ -48,12 +49,10 @@ function Home() {
             console.log(result);
             if (result.status == 200) {
                 setAlltasks(result.data)
-                setEditId(result.data[0].id)
             }
 
         } catch (error) {
             console.log(error);
-
         }
     }
 
@@ -61,31 +60,44 @@ function Home() {
         try {
             const result = await deleteTaskAPI(taskId)
             console.log(result);
+            getTasks()
 
         } catch (error) {
-
+            console.log(error);
         }
     }
-    const handleEdit = (editId) => {
+    const handleEdit = async (editId) => {
+        setEditId(editId)
+        try {
+            const result = await getTaskByIdAPI(editId)
+            setTask(result.data)
+        } catch (error) {
+            console.log(error);
+
+        }
 
         console.log("edit handle", editId);
-
     }
 
     const putEdit = async (editId, editBody) => {
         try {
-            const result = updateTaskAPI(editId, editBody)
+            const result = await updateTaskAPI(editId, editBody)
             console.log(result);
+            setEditId("")
+            setTask({
+                title: "",
+                description: "",
+                status: "pending",
+            })
+            getTasks()
 
         } catch (error) {
             console.log(error);
-
         }
     }
 
     useEffect(() => {
         getTasks()
-        handleEdit()
     }, [])
 
     return (
@@ -100,9 +112,10 @@ function Home() {
                                 :
                                 <span className='text-4xl font-bold py-10'>Add task</span>
                         }
-                        <input onChange={(e) => setTask({ ...task, title: e.target.value })} className='border border-slate-500 py-2 px-5 rounded-2xl' type="text" placeholder='Add task title' />
-                        <input onChange={(e) => setTask({ ...task, description: e.target.value })} className='border border-slate-500 py-2 px-5 rounded-2xl' type="text" placeholder='Add task description' />
-                        {editId &&
+                        <input onChange={(e) => setTask({ ...task, title: e.target.value })} value={task.title} className='border border-slate-500 py-2 px-5 rounded-2xl' type="text" placeholder='Add task title' />
+                        <input onChange={(e) => setTask({ ...task, description: e.target.value })} value={task.description} className='border border-slate-500 py-2 px-5 rounded-2xl' type="text" placeholder='Add task description' />
+                        {
+                            editId &&
                             <select onChange={(e) => setTask({ ...task, status: e.target.value })} className='border border-slate-500 py-2 px-5 rounded-2xl' name="" id="">
                                 <option value="">Select status</option>
                                 <option value="inProgress">In Progress</option>
@@ -111,8 +124,8 @@ function Home() {
                             </select>}
                         {
                             editId ?
-                                <div onClick={handleAdd} className='flex justify-end'>
-                                    <button onClick={setEditId("")} className='border border-slate-500 px-5 py-2 rounded-2xl bg-slate-500 text-white'>
+                                <div className='flex justify-between'>
+                                    <button onClick={() => setEditId("")} className='border border-slate-500 px-5 py-2 rounded-2xl bg-slate-500 text-white'>
                                         Cancel
                                     </button>
                                     <button onClick={() => putEdit(editId, task)} className='border border-slate-500 px-5 py-2 rounded-2xl bg-slate-500 text-white'>
@@ -131,7 +144,7 @@ function Home() {
                 <section className='container gap-10 flex flex-col items-center border border-slate-500 rounded-2xl min-h-[50vh] mt-10'>
                     <h1 className='pt-10 text-4xl'>Task list</h1>
                     <hr className='h-2 w-full text-slate-500 px-5' />
-                    <div className='flex gap-10 justify-start px-20 container'>
+                    <div className='flex flex-wrap gap-10 justify-start px-10 container'>
                         {
                             allTasks.map((item, index) => (
                                 <div key={item.id} className="flex flex-col gap-2 border border-slate-500 rounded-2xl p-5">
--- frontend/src/services/allAPI.js
+++ frontend/src/services/allAPI.js
@@ -8,13 +8,19 @@ export const addTaskAPI = async (reqBody) => {
     return response
 }
 
+// get a task by id
+export const getTaskByIdAPI = async (id) => {
+    const response = await commonAPI("GET",`${serverURL}/todos/${id}`,"")
+    return response
+}
+
 //get all tasks
 export const getTasksAPI = async () => {
     const response = await commonAPI("GET", `${serverURL}/todos`,"")
     return response
 }
 
-//EDIT ALL TASKS
+//EDIT TASKS
 export const updateTaskAPI = async (id,reqBody) => {
     const response = await commonAPI("PUT", `${serverURL}/todos/${id}`,reqBody)
     return response
